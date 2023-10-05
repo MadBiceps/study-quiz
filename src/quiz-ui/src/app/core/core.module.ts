14c7144bd7a782dd8510adfc915ff5409df12365
Fabian Fischer <fabian@fischerdev.com>
2023-10-05 11:18:28 +0800

    Remove footer

--- src/quiz-ui/src/app/core/core.module.ts
+++ src/quiz-ui/src/app/core/core.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { Route, RouterModule } from '@angular/router';
 import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +13,6 @@ import { ClarityModule } from '@clr/angular';
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
   ]
 })
 export class CoreModule { }

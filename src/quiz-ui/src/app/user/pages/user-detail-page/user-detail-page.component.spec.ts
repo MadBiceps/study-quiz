5ca21b4690c103e431c998e58a9de2845f5f632b
Fabian Fischer <fabian@fischerdev.com>
2023-10-08 06:30:39 +0800

    Create user module with user detailed page

---
+++ src/quiz-ui/src/app/user/pages/user-detail-page/user-detail-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserDetailPageComponent } from './user-detail-page.component';
+
+describe('UserDetailPageComponent', () => {
+  let component: UserDetailPageComponent;
+  let fixture: ComponentFixture<UserDetailPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [UserDetailPageComponent]
+    });
+    fixture = TestBed.createComponent(UserDetailPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

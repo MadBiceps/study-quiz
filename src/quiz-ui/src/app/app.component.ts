046e18348f47e97a81296680f763ec0e53094789
Fabian Fischer <fabian@fischerdev.com>
2023-10-24 10:57:19 +0800

    Add route protection and redirect if user is not logged in

--- src/quiz-ui/src/app/app.component.ts
+++ src/quiz-ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';import '@cds/core/icon/register.js';
+import { AuthService } from './services/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';import '@cds/core/icon/register.js';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'quiz-ui';
+  title = 'Study Quiz';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/auth/login']);
+    }
+  }
 }
